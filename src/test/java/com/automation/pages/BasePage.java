4b465f088ce981d86e0f27a32b011ef530e68cff
Srikanth <srikanthtv623@gmail.com>
2026-02-26 05:53:06 +0530

    basicChanges1 commit

--- src/test/java/com/automation/pages/BasePage.java
+++ src/test/java/com/automation/pages/BasePage.java
@@ -1,12 +1,20 @@
 package com.automation.pages;
 
 import com.automation.utils.DriverManager;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
 
     WebDriver driver;
+    private static final int DEFAULT_TIMEOUT = 10;
+    JavascriptExecutor js = (JavascriptExecutor) driver;
 
     public BasePage(){
         this.driver = DriverManager.getDriver();
@@ -14,4 +22,55 @@ public class BasePage {
 
     }
 
+    // WAIT METHODS
+
+    public WebElement waitForElement(WebElement element, int timeoutSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutSeconds));
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public WebElement waitForElement(WebElement element) {
+        return waitForElement(element, DEFAULT_TIMEOUT);
+    }
+
+    // CLICK METHODS
+
+    public void clickElement(WebElement element) {
+        waitForElement(element).click();
+    }
+
+    public void clickElement(WebElement element, int waitTime) {
+        waitForElement(element, waitTime).click();
+    }
+
+    public void advanceClickElement(WebElement element) {
+
+        int maxAttempts = 3;
+
+        for (int i = 1; i <= maxAttempts; i++) {
+            try {
+                waitForElement(element).click();
+                return;
+            }
+            catch (Exception e) {
+
+                System.out.println("Attempt " + i + " failed. Trying JS click...");
+
+                try {
+                    JavascriptExecutor js = (JavascriptExecutor) driver;
+                    js.executeScript("arguments[0].click();", element);
+                    return;
+                }
+                catch (Exception ex) {
+                    if (i == maxAttempts) {
+                        throw new RuntimeException(
+                                "Click failed after " + maxAttempts + " attempts", ex
+                        );
+                    }
+                }
+            }
+        }
+    }
+
 }
+
--- src/test/java/com/automation/utils/DriverManager.java
+++ src/test/java/com/automation/utils/DriverManager.java
@@ -2,22 +2,40 @@ package com.automation.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.time.Duration;
 
 public class DriverManager {
 
-    static WebDriver driver;
+//    static WebDriver driver;
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void createDriver() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+
+        ChromeOptions options = new ChromeOptions();
+
+        // 🔥 SSL Handling
+        options.setAcceptInsecureCerts(true);
+        options.addArguments("--ignore-certificate-errors");
+        options.addArguments("--allow-insecure-localhost");
+
+        driver.set(new ChromeDriver(options));
+        driver.get().manage().window().maximize();
+        driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
     }
 
     public static WebDriver getDriver() {
-        return driver;
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        driver.get().quit();
+        driver.remove();
     }
 
 }
+
+
